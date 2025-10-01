8f88a0d8d4fb8d1ca54661fbafecbda711f4825e
ErnstFinkAG <it@fink-holzbau.ch>
2025-10-01 13:17:24 +0200

    update

--- atlas_copco_mkv/run.sh
+++ atlas_copco_mkv/run.sh
@@ -12,25 +12,38 @@ NAMES="$(bashio::config 'name_list')"
 TYPES="$(bashio::config 'type')"
 TOUTS="$(bashio::config 'timeout_list')"
 
-# Use the first entry from each CSV as a minimal working example.
-# (We can extend to multi-device loop later.)
-IFS=',' read -r IP1 _ <<< "$IPS"
-IFS=',' read -r NAME1 _ <<< "$NAMES"
-IFS=',' read -r TYPE1 _ <<< "$TYPES"
-IFS=',' read -r TOUT1 _ <<< "$TOUTS"
+IFS=',' read -r -a IP_ARR   <<< "$IPS"
+IFS=',' read -r -a NAME_ARR <<< "$NAMES"
+IFS=',' read -r -a TYPE_ARR <<< "$TYPES"
+IFS=',' read -r -a TOUT_ARR <<< "$TOUTS"
 
-if [[ -z "${IP1:-}" || -z "${TYPE1:-}" ]]; then
-  log_error "ip_list and type must have at least one entry"
+count="${#IP_ARR[@]}"
+
+if [[ $count -eq 0 ]]; then
+  log_error "No devices found in ip_list"
   exit 1
 fi
 
-TIMEOUT="${TOUT1:-5}"
+log_info "Found $count device(s) in configuration"
 
-log_info "Polling '$NAME1' ($IP1) with question set '$TYPE1', timeout ${TIMEOUT}s"
-log_info "Command: python3 /atlas_copco_mkv.py --question-set \"$TYPE1\" --controller-host \"$IP1\" --device-name \"$NAME1\" --timeout \"$TIMEOUT\""
+for ((i=0; i<count; i++)); do
+  IP="${IP_ARR[$i]}"
+  NAME="${NAME_ARR[$i]:-Device$i}"
+  TYPE="${TYPE_ARR[$i]:-}"
+  TIMEOUT="${TOUT_ARR[$i]:-5}"
 
-exec python3 /atlas_copco_mkv.py \
-  --question-set "$TYPE1" \
-  --controller-host "$IP1" \
-  --device-name "$NAME1" \
-  --timeout "$TIMEOUT"
+  if [[ -z "$IP" || -z "$TYPE" ]]; then
+    log_error "Skipping index $i: missing ip or type"
+    continue
+  fi
+
+  log_info "Polling '$NAME' ($IP) with question set '$TYPE', timeout ${TIMEOUT}s"
+
+  python3 /atlas_copco_mkv.py \
+    --question-set "$TYPE" \
+    --controller-host "$IP" \
+    --device-name "$NAME" \
+    --timeout "$TIMEOUT"
+done
+
+log_info "Finished polling all devices"
